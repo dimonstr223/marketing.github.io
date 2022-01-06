f40250f582b0a5b7f9cfa73b58557c5e0081698c
dimonstr223 <89186862+dimonstr223@users.noreply.github.com>
2022-01-06 08:21:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+
+  $(".menu a, .go-top").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+
+  $('.slider__blog-inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
+  });
+
+  $('.menu__btn, .menu a').on('click', function(){
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
+});
